f0479ce33a8cc00a8a19bb4287fc79648e077dc5
Igor Emerick <igoremerickbarbosa@hotmail.com>
2021-02-24 14:54:13 -0300

    Conceitos de this, super, equals e hashCode

--- src/main/java/phobosxd/veiculo/Carro.java
+++ src/main/java/phobosxd/veiculo/Carro.java
@@ -3,10 +3,19 @@ package phobosxd.veiculo;
 /**
  * @author PhobosXD
  * @since 2021-02-24
- * @version 1.0
+ * @version 2.0
  */
 public class Carro extends Veiculo {
     private Integer quantidadePortas;
+
+    public Carro(final String marca, final String modelo, final Double valorVenal) {
+        super(marca, modelo, valorVenal);
+    }
+
+    public Carro(final String marca, final String modelo, final Double valorVenal, final Integer quantidadePortas) {
+        super(marca, modelo, valorVenal);
+        this.quantidadePortas = quantidadePortas;
+    }
 
     public void setQuantidadePortas(final Integer quantidadePortas) {
         this.quantidadePortas = quantidadePortas;
--- src/main/java/phobosxd/veiculo/Motocicleta.java
+++ src/main/java/phobosxd/veiculo/Motocicleta.java
@@ -3,10 +3,19 @@ package phobosxd.veiculo;
 /**
  * @author PhobosXD
  * @since 2021-02-24
- * @version 1.0
+ * @version 2.0
  */
 public class Motocicleta extends Veiculo {
     private Integer cilindradas;
+
+    public Motocicleta(final String marca, final String modelo, final Double valorVenal) {
+        super(marca, modelo, valorVenal);
+    }
+
+    public Motocicleta(final String marca, final String modelo, final Double valorVenal, final Integer cilindradas) {
+        super(marca, modelo, valorVenal);
+        this.cilindradas = cilindradas;
+    }
 
     public void setCilindradas(final Integer cilindradas) {
         this.cilindradas = cilindradas;
